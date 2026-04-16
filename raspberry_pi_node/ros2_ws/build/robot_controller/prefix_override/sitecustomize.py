a0867334b562b245e1e23fb392b68306050b5ff3
jeenseo <tjwldls0821@naver.com>
2026-04-16 15:01:56 +0900

    speed_up_test2

--- raspberry_pi_node/ros2_ws/build/robot_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jeen/capstone/Self-delivery_project/raspberry_pi_node/ros2_ws/install/robot_controller'
